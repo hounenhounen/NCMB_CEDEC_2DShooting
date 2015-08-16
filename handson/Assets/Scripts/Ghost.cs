6ee60fc2951e1e3cce3b30156989c74fdf4050cf
hounenhounen <kawahara@kawaharafumishiki-no-MacBook-Air.local>
2015-08-16 14:47:25 +0900

    commit

--- handson/Assets/Scripts/Ghost.cs
+++ handson/Assets/Scripts/Ghost.cs
@@ -7,16 +7,22 @@ using MiniJSON;
 
 public class Ghost : MonoBehaviour {
 	int flameCount = 0;
+	int limit = 0 ;
 	void Start () {
-
+		limit = ((ArrayList)Bg_ghost.posObj["Log"]).Count;
+		//limit = 1000;
+		Debug.Log ("limit:" +limit);
+		
 	}
 	
 	//---Bg_ghost.csで取得したゴーストデータを利用し、ゴーストを操作する----------------------------------------------------
 	void Update () {
-		float x = (float) System.Convert.ToDouble(((ArrayList)((ArrayList)Bg_ghost.posObj["Log"])[flameCount])[0]);
-		float y = (float) System.Convert.ToDouble(((ArrayList)((ArrayList)Bg_ghost.posObj["Log"])[flameCount])[1]);
-		transform.position = new Vector2 (x,y);
-		flameCount ++;
+		if (flameCount < limit) {
+			float x = (float)System.Convert.ToDouble (((ArrayList)((ArrayList)Bg_ghost.posObj ["Log"]) [flameCount]) [0]);
+			float y = (float)System.Convert.ToDouble (((ArrayList)((ArrayList)Bg_ghost.posObj ["Log"]) [flameCount]) [1]);
+			transform.position = new Vector2 (x, y);
+			flameCount ++;
+		}
 	}
 	//-------------------------------------------------------------------------------------------------------------
 }
