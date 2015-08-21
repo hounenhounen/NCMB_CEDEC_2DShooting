2d955995f21fef5701342276f1584f0441f58f81
hounenhounen <hounen.hounen@gmail.com>
2015-08-21 18:31:21 +0900

    Commit

--- handson/Assets/Scripts/SaveScore.cs
+++ handson/Assets/Scripts/SaveScore.cs
@@ -7,15 +7,8 @@ public class SaveScore : MonoBehaviour {
 	// mobile backendに接続------------------------
 	public void save( string name, int score ) {
 		NCMBObject obj = new NCMBObject ("Score");
-		obj.Add ("name", name);//オブジェクトに名前とスコアを設定
-		obj.Add ("score", score);
-		obj.SaveAsync ((NCMBException e) => {      
-			if (e != null) {
-				//エラー処理
-			} else {
-				//成功時の処理
-			}                  
-		
-		});//この処理でサーバーに書き込む
+		obj ["name"] = name;//オブジェクトに名前とスコアを設定
+		obj ["score"] = score;
+		obj.SaveAsync ();//この処理でサーバーに書き込む
 	}
 }
